a58d1b056d0603e33bb7931c5d0beb7c7fdd79c8
sajeewa9930 <sajeewaml@gmail.com>
2024-04-08 17:43:14 +0530

    fixed bug

--- components/application.tsx
+++ components/application.tsx
@@ -8,33 +8,35 @@ const Application = () => {
   const repeatImages = Array.from({ length: repeatFactor }, () => applicationImages).flat();
 
   return (
-      <section
-          className="py-10 px-10 md:px-40 flex flex-col items-center justify-center gap-10 bg-gray-100 scroll-mt-20 dark:bg-gray-700"
-          id="application"
-      >
-        <h2 className={`${fjalla.className} text-3xl md:text-5xl font-semibold text-[#95A5A6]`}>
-          Our Applications
-        </h2>
-        <div className="overflow-hidden h-[92px] w-full relative">
-          <div className="w-[4000px] absolute top-0 left-0 bottom-0 flex items-center justify-between flex-nowrap translate- gap-10 marquee-Application">
-            {repeatImages.map((item, index) => (
-                <React.Fragment key={index}>
-                  {/* Wrap each Image with an anchor tag */}
-                  {/* <a href={item.websiteUrl} target="_blank" rel="noopener noreferrer"> */}
-                    <Image
-                        src={item.imgUrl}
-                        alt={item.alt}
-                        width={100}
-                        height={100}
-                        className="w-auto h-auto object-contain"
-                    />
-                    <p className="text-xs">{item.name}</p> {/* Add name below the image */}
-                  {/* </a> */}
-                </React.Fragment>
-            ))}
-          </div>
+    <section
+      className="py-10 px-10 md:px-40 flex flex-col items-center justify-center gap-10 bg-gray-100 scroll-mt-20 dark:bg-gray-700"
+      id="application"
+    >
+      <h2 className={`${fjalla.className} text-3xl md:text-5xl font-semibold text-[#95A5A6]`}>
+        Our Applications
+      </h2>
+      <div className="overflow-hidden h-[92px] w-full relative">
+        <div className="w-[4000px] absolute top-0 left-0 bottom-0 flex items-center justify-between flex-nowrap translate- gap-10 marquee-Application">
+          {repeatImages.map((item, index) => (
+            <React.Fragment key={index}>
+              {/* Wrap each Image with an anchor tag */}
+              {/* <a href={item.websiteUrl} target="_blank" rel="noopener noreferrer"> */}
+              <div className="flex flex-col items-center justify-center">
+                <Image
+                  src={item.imgUrl}
+                  alt={item.alt}
+                  width={100}
+                  height={100}
+                  className="w-auto h-auto object-contain"
+                />
+                <p className="text-xs">{item.name}</p> {/* Add name below the image */}
+              </div>
+              {/* </a> */}
+            </React.Fragment>
+          ))}
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 
