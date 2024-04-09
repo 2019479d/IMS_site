e49f99fbb1e350551a5b3f39b91ccae668f8ea2e
sajeewa9930 <sajeewaml@gmail.com>
2024-04-09 23:52:18 +0530

    add

--- components/application.tsx
+++ components/application.tsx
@@ -19,7 +19,7 @@ const Application = () => {
         <div className="w-[4000px] absolute top-0 left-0 bottom-0 flex items-center justify-between flex-nowrap translate- gap-10 marquee-application">
           {repeatImages.map((item, index) => (
             <React.Fragment key={index}>
-              <div className="flex flex-col h-[200px] items-center justify-center">
+              <div className="flex flex-col h-[200px] w-[200px] items-center justify-center">
                 <Image
                   src={item.imgUrl}
                   alt={item.alt}
