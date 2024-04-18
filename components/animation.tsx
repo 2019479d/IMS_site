cb9ad8611b2e18f70709f732bfdf577cfca575e3
2019479d <62051752+2019479d@users.noreply.github.com>
2024-04-18 20:17:21 +0530

    powerGeneration change to Testing- Update animation.tsx

--- components/animation.tsx
+++ components/animation.tsx
@@ -142,7 +142,7 @@ const Animation = () => {
 
           />
           <span className="font-semibold text-white text-sm">Monitoring</span>
-        </div>
+        </div>`
 
         <div className="z-10 absolute bottom-[25%] left-[40%] h-fit w-fit flex flex-col items-center justify-center">
           <Image
@@ -154,7 +154,7 @@ const Animation = () => {
             style={{ opacity: 0.42 }} // Adjust the opacity value as needed (from 0 to 1)
 
           />
-          <span className="font-semibold text-white">Power Generation</span>
+          <span className="font-semibold text-white">Testing</span>
         </div>
         <div className="-z-1 absolute bottom-[45%] left-[20%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] plane-anim-1"></div>
         <div className="-z-1 absolute bottom-[42%] left-[23.5%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] plane-anim-2"></div>
